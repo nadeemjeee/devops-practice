6c5ac6bc040e5734e5bf9a793467eb2c4e832ffe
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-04-19 22:21:37 +0200

    added new file

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        int num1 =1;
+        int num =2;
+        int sum = num+num1;
+
+        System.out.println(sum);
+    }
+}
