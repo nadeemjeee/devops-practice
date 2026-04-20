18e5ea48976dc490089fa22ee73e01c698ba5170
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-04-20 19:13:44 +0200

    added new files and branch

---
+++ work.java
@@ -0,0 +1,9 @@
+public class work {
+    public static void main(String[] args) {
+        String fname = "Nadeem";
+        String lname = "Ahmad";
+        System.err.println("His name is "+ fname+ " "+lname);
+
+
+    }
+}
